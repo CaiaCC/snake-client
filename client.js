29e312772c9d78245c62c42a82efd5c69259fb72
Caia Chuang <caiachung@gmail.com>
2020-07-29 19:46:37 +0000

    Move the connect function from play.js into client.js

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+
+const connect = function() {
+  const conn = net.createConnection({ 
+		host:'135.23.222.131',
+		port: 50541
+	});
+	conn.setEncoding('utf8'); 
+
+	conn.on('data', (data) => {
+		console.log('Server says: ', data);
+	});
+
+  conn.on('connect', () => {
+	  conn.write('Caia Snake');
+	
+	});
+	
+  return conn;
+}
+
+module.exports = connect
--- play.js
+++ play.js
@@ -1,25 +1,5 @@
-const net = require('net');
-// const stdin = require('process');
-// stdin.setEncoding('utf-8');
+const connect = require('./client');
 
-const connect = function() {
-  const conn = net.createConnection({ 
-		host:'135.23.222.131',
-		port: 50541
-	});
-	conn.setEncoding('utf8'); 
-
-	conn.on('data', (data) => {
-		console.log('Server says: ', data);
-	});
-
-  conn.on('connect', () => {
-	  conn.write('La La La LHL Life');
-	
-	});
-	
-  return conn;
-}
 
 console.log('Connecting ...');
 connect();
